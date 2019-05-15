bf6789d9fca2cfa97ac33753ce969e89045dd277
Okan Konur <okan@konur.com>
2019-05-15 20:46:19 +0300

    re-commit

---
+++ FormatSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using iTextSharp;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace EksiXMLtoPDF
+{
+    class FormatSettings
+    {
+        public static BaseFont FontBase = iTextSharp.text.pdf.BaseFont.CreateFont("Helvetica", "Cp1254", iTextSharp.text.pdf.BaseFont.NOT_EMBEDDED);
+        public static Font fontNormal = new Font(FontBase, 12, Font.NORMAL, BaseColor.DARK_GRAY);
+        public static Font fontHeading = new Font(FontBase, 18, Font.UNDERLINE | Font.BOLD, BaseColor.BLACK);
+        public static Font fontInfo = new Font(FontBase, 8, Font.ITALIC, BaseColor.GRAY);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using iTextSharp;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace EksiXMLtoPDF
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            XDocument xdoc = XDocument.Load(args[0]);
+            Document myDocument = new Document(PageSize.A4, 25, 25, 30, 30);
+
+            try
+            {
+                using (Stream outStream = new FileStream(args[0].Split('.')[0] + ".pdf", FileMode.OpenOrCreate))
+                {
+                    PdfWriter myPDFWriter = PdfWriter.GetInstance(myDocument, outStream);
+                    myDocument.Open();
+
+                    foreach (var element in xdoc.Descendants("entry"))
+                    {
+                        Paragraph title = new Paragraph(element.Attribute("title").Value,
+                                                                FormatSettings.fontHeading);
+                        title.Alignment = 0;
+                        title.FirstLineIndent = 1;
+                        title.SpacingAfter = 2;
+                        Paragraph entry = new Paragraph(element.Value,
+                                                                FormatSettings.fontNormal);
+                        entry.Alignment = 0;
+                        entry.FirstLineIndent = 1;
+                        Paragraph info = new Paragraph("id: " + element.Attribute("id").Value +
+                                                                " tarih: " + element.Attribute("date").Value,
+                                                                FormatSettings.fontInfo);
+                        info.Alignment = 2;
+                        info.SpacingAfter = 2;
+
+                        myDocument.Add(title);
+                        myDocument.Add(entry);
+                        myDocument.Add(info);
+                    }
+
+
+                    myDocument.Close();
+                    myPDFWriter.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
